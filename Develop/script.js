dff858481a47593bb5ec50e6a675f3f2c458d2e9
JD Marty <joshua.dmarty@gmail.com>
2020-10-14 18:46:50 -0700

    added toggles to change date in jumbotron

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,26 @@
 //globals
 var now = moment()
-var currentYear = moment().year()
-var currentMonth = moment().month()
-var currentDayOf = moment().date()
-var currentHour = moment().hour()
+var currentYear = now.year()
+var currentMonth = now.month()
+var currentDayOf = now.date()
+var currentHour = now.hour()
 
 //function to display todays date at the top of the page
-function setToday() {
-    $('#currentDay').text('Today is: '+moment().format('dddd, MMM Do YYYY'))
+function setDate(day) {
+    $("#currentDay")
+      .text("Date: " + day.format("dddd, MMM Do YYYY"))
+      .append("<i class='fas fa-lg fa-caret-right mx-4'></i>")
+      .prepend("<i class='fas fa-lg fa-caret-left mx-4'></i>");
 }
-setToday()
+setDate(now)
 
 //function to generate calendar
 function generateCalendar() {
     var hour = 0
     var thisDay = moment() //will be an input
     while (hour < 24) {
-        //create a date for this hour
-        var thisRowHour = moment(new Date(thisDay.year(), thisDay.month(), thisDay.date(), hour))
+        //create a moment for this hour
+        var thisRowMoment = moment(new Date(thisDay.year(), thisDay.month(), thisDay.date(), hour))
         //set hour display
         var hourString = ''
         hour > 12 ? hourString = hour-12 + 'PM' : hourString = hour + 'AM'
@@ -25,17 +28,18 @@ function generateCalendar() {
         if (hour === 12) hourString = '12PM';
         //set past/present/future
         var timeClass
-        if (thisRowHour.isBefore(now)) timeClass = 'past';
-        if (thisRowHour.isAfter(now)) timeClass = 'future';
-        if (thisRowHour.isSame(now, 'hour')) timeClass = 'present'
+        if (thisRowMoment.isBefore(now)) timeClass = 'past';
+        if (thisRowMoment.isAfter(now)) timeClass = 'future';
+        if (thisRowMoment.isSame(now, 'hour')) timeClass = 'present'
         //create and append blocks for hour, time and save
         var newHourBlock = $('<div>')
             .addClass('col-1 hour align-center text-right')
             .html(`<p class='py-3'>${hourString}</p>`)
-        var newTextArea = $('<textarea>')
-            .addClass(`col-10 ${timeClass}`)
-        var newSaveButton = $('<div>')
-            .addClass('col-1 saveBtn')
+        var newTextArea = $("<textarea>")
+          .addClass(`col-10 ${timeClass}`)
+        var newSaveButton = $("<div>")
+          .addClass("col-1 saveBtn")
+          .html('<i class="fas fa-save py-3"></i>');
         var newRow = $('<div>')
             .addClass('row time-block')
             .attr('id', `hour_${hour}`)
@@ -43,16 +47,10 @@ function generateCalendar() {
             .append(newTextArea)
             .append(newSaveButton)
         $('#schedule').append(newRow)
-        //scroll to the current hour
-        if (thisRowHour.isSame(now, "hour")) {
-            $("#schedule").animate(
-                { scrollTop: $(`#hour_${currentHour}`).offset().top, },
-                1000
-            );
-        }
-        
         hour++
     }
+    console.log($(`#hour_${currentHour}`));
+    $(`#hour_${currentHour}`).children('textarea').focus();
 }
 
 generateCalendar()
